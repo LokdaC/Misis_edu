67641f699f46e2f318bfbebee8225c1b7cc7cc47
Влад Сухомлинов <mr.svladisl@mail.ru>
2025-07-03 16:28:18 +0700

    Выполнил ДЗ 1

---
+++ HomeWork1.py
@@ -0,0 +1,65 @@
+# # Задание 1
+# example_str = '***---Добро пожаловать!---***'
+# a=example_str.strip('***---')
+# print (a)
+
+# # Задание 2
+# example_str = 'В строке, есть, ошибка, снова ошибка.'
+# a=example_str.replace (',','.')
+# b=a.replace ('ошибка', 'исправление')
+# print (b)
+
+# # Задание 3
+# example_str = 'Иванов;Петров;Сидоров;Кузнецов'
+# a=example_str.split (';')
+# b=list (a)
+# c=b[1]
+# print (c)
+
+#  Задание 4
+# words = ['Скоро', 'будет', 'контрольная']
+# a=' '.join(words) + '.'
+# print (a)
+
+#Задание 5
+# nums = [4, 2, 9, 0, 4, 7]
+# nums.remove (4)
+# nums.sort ()
+# print (nums)
+
+# Задание 6
+# numbers = [10, 20, 30, 40]
+# numbers.pop (3)
+# print (numbers)
+
+# Задание 7
+# numbers = [5, 2, 9, 1]
+# numbers.sort ()
+# print (numbers)
+
+# Задание 8
+# nums = [3, 1, 3, 7, 3, 2]
+# result=nums.count (3)
+# print (result)
+
+# Задание 9
+# a = ('RGB', 255, 0, 128) 
+# mode, r, g, b = a
+# print (mode)
+# print (r)
+# print (g)
+# print (b)
+
+# Задание 10
+# dict =  {'a': 1, 'b': 2}
+# dict ['c'] = 3
+# dict ['a'] = 1+5
+# print (dict)
+
+# Задание 11
+# person = {'имя': 'Аня', 'город': 'Тверь'}
+# print(person ['имя'])
+
+# Задание 12
+# person = {'имя': 'Игорь'}
+# print(person.get('номер телефона', 'неизвестно')) 
