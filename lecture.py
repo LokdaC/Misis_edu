4ad83eb029e74006475990174cbfbaddc48712ef
Влад Сухомлинов <mr.svladisl@mail.ru>
2025-07-04 17:19:50 +0700

    Встреча 1

---
+++ lecture.py
@@ -0,0 +1,34 @@
+#Задание 0
+#a="Кабачки полезны для здоровья."
+#print(a[0])
+#print (a[10])
+#print (a[1])
+#print (a[-8])
+
+# #Задание 1
+# b='Snake_Case'
+# result1=b.replace('_','')
+# result=result1.replace('Snake','Camel')
+# print (result)
+
+# c ='max_value_of_array'
+# result1=c.replace ('_',' ')
+# result2=str.title (result1)
+# result3=str.split(result2)
+# result4=str.lower (result3[0])+result3[1]+result3[2]+result3[3]
+# print (result4)
+
+#Задание 2
+# numbers = [1,4,4,6,7,8,9,5,5]
+# numbers.remove(4)
+# numbers.remove(5)
+# print (numbers)
+
+# #Задание 3
+# Orders=(3000,4000,2450,4560,9876,1546,4567,3498,4563,1345,3456,3450)
+# max1=max(Orders) 
+# index1=Orders.index(max1)
+# max2=max(Orders) 
+# index2=Orders.index(max2)
+# Доделать 
+# print (index1)
